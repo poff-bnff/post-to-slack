246e563c03f1ff9bac4da5d8f7f86fcb264a6c00
Mariann <tapferm@gmail.com>
2020-10-13 11:32:07 +0300

    added some inputs

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const ToSlack = require("./postToHook");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const user = core.getInput('slackUserId');
-  console.log(`Hello ${user}!`);
+  const user = core.getInput('user');
+  console.log(`User: ${user}!`);
+  const channel = core.getInput('privateChannel');
+  console.log(`Channel: ${user}!`);
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
@@ -14,16 +16,48 @@ try {
 
   const payload = JSON.stringify(github.context.payload, undefined, 2)
 
+
+  const status = core.getInput("status");
+  let statusText = getText(status)
+
   let data ={
-    "user": payload.inputs.user,
-    "sender": payload.sender.login
+    "user": user,
+    "sender": payload.sender.login,
+    "channel": channel,
+    // "commit": payload,
+    // "action": payload,
+    // "workflow": payload,
+
   }
 
-  console.log("see on minu action olemas")
+  console.log("see on minu action muutustega")
   console.log(`The event payload: ${payload}`);
 
   ToSlack.POST(data)
 
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function getText(status) {
+  const actor = github.context.actor;
+  const workflow = github.context.workflow;	
+  started = `<http://github.com/${actor}|${actor}>` + ' has *started* the "' + `${workflow}`  + '"' + ' workflow ';
+  succeeded = 'The workflow "' + `${workflow}` + '"' + ' was completed *successfully* by ' + `<http://github.com/${actor}|${actor}>`;
+  cancelled = ':warning: The workflow "' + `${workflow}` + '"' + ' was *canceled* by ' + `<http://github.com/${actor}|${actor}>`;
+  failure = '<!here> The workflow "' + `${workflow}` + '"' + ' *failed*';
+  
+  if (status.toLowerCase() === 'success') {
+      return succeeded;
+  }
+  if (status.toLowerCase() === 'cancelled') {
+      return cancelled;
+  }
+  if (status.toLowerCase() === 'failure') {
+      return failure;
+  }
+  if (status.toLowerCase() === 'started') {
+      return started;
+  }
+  return 'status no valido';
 }
